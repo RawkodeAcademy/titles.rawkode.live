894031ed0532eb4d54ec595b63bd8fae75476a8e
David Flanagan <david@rawkode.com>
2021-12-22 17:50:49 +0000

    feat: initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,42 @@
+<section class="inline-flex p-4">
+	<img class="logo" src="img/logo.svg" alt="Rawkode logo" />
+</section>
+
+<section class="flex text-center justify-center items-start mt-2 w-full">
+	<h1
+		class="text-6xl tracking-wide italic font-semibold p-12 bg-white opacity-95 text-black mix-blend-screen border-8 m-7 border-black "
+	>
+		Pulumi: Multi Language Packages
+	</h1>
+</section>
+
+<section class="flex align-bottom justify-center items-stretch flex-row h-screen">
+	<div class="flex absolute bottom-0 overflow-hidden">
+		<img
+			class="relative z-30 inline-block h-32 w-32 rounded-full ring-2 ring-white"
+			src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+			alt=""
+		/>
+		<img
+			class="relative z-20 inline-block h-32 w-32 rounded-full ring-2 ring-white"
+			src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+			alt=""
+		/>
+		<img
+			class="relative z-10 inline-block h-32 w-32 rounded-full ring-2 ring-white"
+			src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+			alt=""
+		/>
+		<img
+			class="relative z-0 inline-block h-32 w-32 rounded-full ring-2 ring-white"
+			src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+			alt=""
+		/>
+
+		<img
+			class="relative z-0 inline-block h-32 w-32 rounded-full ring-2 ring-fuchsia-600"
+			src="img/pulumi.png"
+			alt=""
+		/>
+	</div>
+</section>
